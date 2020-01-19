5d69faff11de3b337b9acaac1675ef99eed2d6cf
ElenaZht <elenazht@gmail.com>
2020-01-19 22:51:26 +0200

    home-page component: -html and css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import {UsersService} from './users.service';
+import {UsersArrayService} from './users-array.service';
+import {FormsModule} from '@angular/forms';
 import { HomePageComponent } from './home-page/home-page.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: UsersService, useClass: UsersArrayService}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
