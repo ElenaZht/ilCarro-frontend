6d64598c1f2a3be32af532cedb553cd53a6dd89c
ElenaZht <elenazht@gmail.com>
2020-02-11 16:32:39 +0200

    home-page component: -html and css complete

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {User, UsersService} from '../users.service';
-import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,32 +6,10 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-  checkboxError: boolean;
-  errorText: string;
-  constructor(private usersService: UsersService) {
-    this.checkboxError = false;
-    this.errorText = '';
-  }
-  ngOnInit() {}
 
-  onSubmit(signUpForm: NgForm) {
-    console.log(signUpForm.value.check);
-    this.checkboxError = !signUpForm.value.check ;
-    if (this.checkboxError) {
-      return;
-    }
-    const user = signUpForm.value as User;
-    console.log(' то место', user);
-    this.usersService.AddUser(user)
-      .subscribe(res => {
-        if (res) {
-          signUpForm.reset();
-        }
-      }, err => {
-        this.errorText = err;
-        console.log(err);
-      });
+  constructor() { }
+
+  ngOnInit() {
   }
+
 }
-
-
