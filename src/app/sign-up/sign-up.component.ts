8654f3782dd389279869a1e7bb09c33746645149
ElenaZht <elenazht@gmail.com>
2020-02-18 23:34:31 +0200

    sign-up component: -html and css complete -ts and mimic complete -added user service (addUser)

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {User, UsersService} from '../users.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  checkboxError: boolean;
+  errorText: string;
+  constructor(private usersService: UsersService) {
+    this.checkboxError = false;
+    this.errorText = '';
   }
+  ngOnInit() {}
 
+  onSubmit(signUpForm: NgForm) {
+    console.log(signUpForm.value.check);
+    this.checkboxError = !signUpForm.value.check ;
+    if (this.checkboxError) {
+      return;
+    }
+    const user = signUpForm.value as User;
+    console.log(' то место', user);
+    this.usersService.AddUser(user)
+      .subscribe(res => {
+        if (res) {
+          signUpForm.reset();
+        }
+      }, err => {
+        this.errorText = err;
+        console.log(err);
+      });
+  }
 }
+
+
